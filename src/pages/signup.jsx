6aa38fe3188ed662848aa1e85a95779e9bab41ff
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-06-07 17:02:50 -0300

    feat: ensure local storage tokens are used to reauthenticated on signup page

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,6 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useMutation } from '@tanstack/react-query'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
@@ -88,6 +88,33 @@ const SignupPage = () => {
       terms: false,
     },
   })
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const accessToken = localStorage.getItem('accessToken')
+        const refreshToken = localStorage.getItem('refreshToken')
+
+        if (!accessToken && !refreshToken) {
+          return
+        }
+
+        const response = await api.get('/users/me', {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+
+        setUser(response.data)
+      } catch (error) {
+        localStorage.removeItem('accessToken')
+        localStorage.removeItem('refreshToken')
+        console.log(error)
+      }
+    }
+
+    init()
+  }, [])
 
   const handleSubmit = (data) => {
     signupMutation.mutate(data, {
