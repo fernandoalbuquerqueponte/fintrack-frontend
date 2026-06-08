d775b67eebb1f3b539431b398d1eaa02341664f4
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-06-08 15:22:34 -0300

    feat: add protected and public api instances

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -6,7 +6,7 @@ import {
   LOCAL_STORAGE_ACCESS_TOKEN_KEY,
   LOCAL_STORAGE_REFRESH_TOKEN_KEY,
 } from '@/constants/local-storage'
-import { api } from '@/lib/axios'
+import { protectedApi, publicApi } from '@/lib/axios'
 
 export const AuthContext = createContext({
   user: null,
@@ -35,7 +35,7 @@ export const AuthContextProvider = ({ children }) => {
   const signupMutation = useMutation({
     mutationKey: ['signup'],
     mutationFn: async (variables) => {
-      const response = await api.post('/users', {
+      const response = await publicApi.post('/users', {
         first_name: variables.first_name,
         last_name: variables.last_name,
         email: variables.email,
@@ -48,7 +48,7 @@ export const AuthContextProvider = ({ children }) => {
   const signinMutation = useMutation({
     mutationKey: ['login'],
     mutationFn: async (data) => {
-      const response = await api.post('/users/login', {
+      const response = await publicApi.post('/users/login', {
         email: data.email,
         password: data.password,
       })
@@ -68,7 +68,7 @@ export const AuthContextProvider = ({ children }) => {
         if (!accessToken && !refreshToken) {
           return
         }
-        const response = await api.get('/users/me')
+        const response = await protectedApi.get('/users/me')
 
         setUser(response.data)
       } catch (error) {
