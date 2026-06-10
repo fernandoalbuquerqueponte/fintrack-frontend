81f54ef4eafc0332ccc0819f255e8e2947173920
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-06-10 11:19:57 -0300

    fix: ensure user balance query is properly invalidate

--- src/api/hooks/user.js
+++ src/api/hooks/user.js
@@ -3,12 +3,12 @@ import { useQuery } from '@tanstack/react-query'
 import { UserService } from '@/api/services/user'
 import { useAuthContext } from '@/contexts/auth'
 
-export const getUserBalanceQueryKey = ({ userId, from, to }) => [
-  'balance',
-  userId,
-  from,
-  to,
-]
+export const getUserBalanceQueryKey = ({ userId, from, to }) => {
+  if (!from || !to) {
+    return ['balance', userId]
+  }
+  return ['balance', userId, from, to]
+}
 
 export const useGetUserBalance = ({ from, to }) => {
   const { user } = useAuthContext()
