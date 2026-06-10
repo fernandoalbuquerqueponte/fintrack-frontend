2b8a089186f275253adba06b9d4e3a36ed31b3fe
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-06-10 17:22:23 -0300

    feat: ensure transaction can be edited

---
+++ src/components/edit-transaction-button.jsx
@@ -0,0 +1,189 @@
+import {
+  ExternalLinkIcon,
+  Loader2Icon,
+  PiggyBankIcon,
+  TrendingDownIcon,
+  TrendingUpIcon,
+} from 'lucide-react'
+import { useState } from 'react'
+import { NumericFormat } from 'react-number-format'
+import { toast } from 'sonner'
+
+import { useEditTransactionForm } from '@/forms/hooks/transaction'
+
+import { Button } from './ui/button'
+import { DatePicker } from './ui/date-picker'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form'
+import { Input } from './ui/input'
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetFooter,
+  SheetTitle,
+  SheetTrigger,
+} from './ui/sheet'
+
+const EditTransactionButton = ({ transaction }) => {
+  const [sheetIsOpen, setSheetIsOpen] = useState(false)
+  const { form, onSubmit } = useEditTransactionForm({
+    transaction,
+    onSuccess: () => {
+      setSheetIsOpen(false)
+      toast.success('Transação editada com sucesso!')
+    },
+    onError: () => {
+      toast.error('Erro ao editar transação. Por favor, tente novamente.')
+    },
+  })
+  return (
+    <Sheet open={sheetIsOpen} onOpenChange={setSheetIsOpen}>
+      <SheetTrigger asChild>
+        <Button variant="ghost" size="icon">
+          <ExternalLinkIcon className="text-muted-foreground" />
+        </Button>
+      </SheetTrigger>
+      <SheetContent className="min-w-[450px]">
+        <SheetTitle>Editar Transação</SheetTitle>
+        <Form {...form}>
+          <form className="space-y-8" onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="Digite o nome da transação"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field: { onChange, ...restField } }) => (
+                <FormItem>
+                  <FormLabel>Valor</FormLabel>
+                  <FormControl>
+                    <NumericFormat
+                      {...restField}
+                      placeholder="Digite o valor da transação"
+                      thousandSeparator="."
+                      decimalSeparator=","
+                      prefix="R$ "
+                      allowNegative={false}
+                      customInput={Input}
+                      onValueChange={(values) => {
+                        onChange(values.floatValue || 0)
+                      }}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="date"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Data</FormLabel>
+                  <FormControl>
+                    <DatePicker
+                      {...field}
+                      placeholder="Selecione a data da transação"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="type"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Tipo</FormLabel>
+                  <FormControl>
+                    <div className="grid grid-cols-3 gap-4">
+                      <Button
+                        type="button"
+                        variant={
+                          field.value === 'EARNING' ? 'secondary' : 'outline'
+                        }
+                        onClick={() => field.onChange('EARNING')}
+                      >
+                        <TrendingUpIcon className="text-primary-green" />
+                        Ganho
+                      </Button>
+                      <Button
+                        type="button"
+                        variant={
+                          field.value === 'EXPENSE' ? 'secondary' : 'outline'
+                        }
+                        onClick={() => field.onChange('EXPENSE')}
+                      >
+                        <TrendingDownIcon className="text-primary-red" />
+                        Gasto
+                      </Button>
+                      <Button
+                        type="button"
+                        variant={
+                          field.value === 'INVESTMENT' ? 'secondary' : 'outline'
+                        }
+                        onClick={() => field.onChange('INVESTMENT')}
+                      >
+                        <PiggyBankIcon className="text-primary-blue" />
+                        Investimento
+                      </Button>
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <SheetFooter className="sm:space-x-4">
+              <SheetClose asChild>
+                <Button
+                  disabled={form.formState.isSubmitting}
+                  type="reset"
+                  variant="secondary"
+                  className="w-full"
+                >
+                  Cancelar
+                </Button>
+              </SheetClose>
+              <Button
+                disabled={form.formState.isSubmitting}
+                type="submit"
+                className="w-full"
+              >
+                {form.formState.isSubmitting && (
+                  <Loader2Icon className="animate-spin" />
+                )}
+                Salvar
+              </Button>
+            </SheetFooter>
+          </form>
+        </Form>
+      </SheetContent>
+    </Sheet>
+  )
+}
+
+export default EditTransactionButton
